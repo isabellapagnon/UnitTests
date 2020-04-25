437e46ef0c99f1d3cd049a9a997c961ffb828aad
Isabella Pagnoncelli <isabella.pagnoncelli@hotmail.com>
2020-04-25 18:05:54 -0300

    ContaMagica feito

--- TrabalhoTeste/src/main/java/com/UnitTest/ContaMagica.java
+++ TrabalhoTeste/src/main/java/com/UnitTest/ContaMagica.java
@@ -11,16 +11,37 @@ public class ContaMagica {
     }
 	
     int getStatus(){
+        if (saldo <= 50000){
+            status = SILVER;
+        }
+        if (saldo > 50000 && saldo < 200000){
+            status = GOLD;
+        }
+        if (saldo > 200000){
+            status = PLATINUM;
+        }
         return status;
     }
 	
     void deposito(int valor) {
-        //throws INVALID_OPER_EXCEPTION
+        if (valor < 0) {
+            //throws INVALID_OPER_EXCEPTION
+            System.out.println("Valor inválido!");  
+        }
+        else {
+            saldo = saldo + valor;
+        }
         
-    };
+    }
 
     void retirada(int valor) {
-       // throws INVALID_OPER_EXCEPTION{
+        if (valor < 0) {
+            //throws INVALID_OPER_EXCEPTION
+            System.out.println("Valor inválido!");  
+        }
+        else {
+            saldo = saldo - valor;
+        }
 
-    };
+    }
 }
