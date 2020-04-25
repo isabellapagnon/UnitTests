4c2a42f4600cc66f8713a3c77a30324153639a7e
Isabella Pagnoncelli <isabella.pagnoncelli@hotmail.com>
2020-04-25 15:59:22 -0300

    Project with mvn created

---
+++ TrabalhoTeste/src/main/java/com/UnitTest/ContaMagica.java
@@ -0,0 +1,24 @@
+package com.UnitTest;
+public class ContaMagica {
+    public static final int SILVER = 0;
+    public static final int GOLD = 1;
+    public static final int PLATINUM = 2;
+    
+    double getSaldo(){
+        return -1;
+    }
+	
+    int getStatus(){
+        return -1;
+    }
+	
+    void deposito(int valor) {
+        //throws INVALID_OPER_EXCEPTION
+        
+    };
+
+    void retirada(int valor) {
+       // throws INVALID_OPER_EXCEPTION{
+
+    };
+}
