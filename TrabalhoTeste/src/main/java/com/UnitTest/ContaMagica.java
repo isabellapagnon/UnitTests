698d9ddcc9df35f5d01f71dea594d30a90034120
Isabella Pagnoncelli <isabella.pagnoncelli@hotmail.com>
2020-04-26 10:21:45 -0300

    Testes deposito feitos

--- TrabalhoTeste/src/main/java/com/UnitTest/ContaMagica.java
+++ TrabalhoTeste/src/main/java/com/UnitTest/ContaMagica.java
@@ -3,8 +3,8 @@ public class ContaMagica {
     public static final int SILVER = 0;
     public static final int GOLD = 1;
     public static final int PLATINUM = 2;
-    double saldo;
-    int status;
+    double saldo = 0;
+    int status = SILVER;
     
     double getSaldo(){
         return saldo;
@@ -14,7 +14,7 @@ public class ContaMagica {
         if (saldo <= 50000){
             status = SILVER;
         }
-        if (saldo > 50000 && saldo < 200000){
+        if (saldo > 50000 && saldo <= 200000){
             status = GOLD;
         }
         if (saldo > 200000){
@@ -23,9 +23,10 @@ public class ContaMagica {
         return status;
     }
 	
-    void deposito(int valor) {
+    void deposito(int valor) //throws INVALID_OPER_EXCEPTION 
+    {
         if (valor < 0) {
-            //throws INVALID_OPER_EXCEPTION
+            //throw new Exception();
             System.out.println("Valor inválido!");  
         }
         else {
@@ -43,5 +44,17 @@ public class ContaMagica {
             saldo = saldo - valor;
         }
 
+    }
+    public static void main(String args[]){
+        ContaMagica conta = new ContaMagica();
+        System.out.println("----------------//");
+        conta.deposito(25000);
+        System.out.println(conta.getSaldo());
+        System.out.println(conta.getStatus());
+        conta.deposito(30000);
+        System.out.println(conta.getSaldo());
+        System.out.println(conta.getStatus());
+
+        System.out.println("-------- FIM --------");
     }
 }
--- TrabalhoTeste/src/test/java/com/UnitTest/ContaMagicaTest.java
+++ TrabalhoTeste/src/test/java/com/UnitTest/ContaMagicaTest.java
@@ -1,19 +1,63 @@
 package com.UnitTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class ContaMagicaTest {
 
-    public void depositoSilverParaGold(){}
+    @Test
+    public void silver_Com_0_Saldo(){
+        ContaMagica c = new ContaMagica();
+        c.deposito(0);
+        assertEquals(c.getStatus(), 0);
+    }
 
-    public void depositoSilverParaPlatinium(){}
+    @Test
+    public void silver_Com_50000_Saldo(){
+        ContaMagica c = new ContaMagica();
+        c.deposito(50000);
+        assertEquals(c.getStatus(), 0);
+    }
 
+    @Test
+    public void deposito_Silver_Para_Gold_50001_Saldo(){
+        ContaMagica c = new ContaMagica();
+        c.deposito(50001);
+        assertEquals(c.getStatus(), 1);
+    }
+
+    @Test
+    public void deposito_Silver_Para_Gold_200000_Saldo(){
+        ContaMagica c = new ContaMagica();
+        c.deposito(200000);
+        assertEquals(c.getStatus(), 1);
+    }
+
+    @Test
+    public void deposito_Gold_Para_Platinium_200001_Saldo(){
+        ContaMagica c = new ContaMagica();
+        c.deposito(200001);
+        assertEquals(c.getStatus(), 2);
+    }
+
+    @Test
+    public void deposito_Gold_Para_Platinium_1000000_Saldo(){
+        ContaMagica c = new ContaMagica();
+        c.deposito(1000000);
+        assertEquals(c.getStatus(), 2);
+    }
+
+    @Test
     public void retiradaPlatiniumparaGold() {} 
 
+    @Test
     public void retiradaGoldparaSilver(){}
 
-    public void saldoInsuficiente(){}
-
-    public void depositoNegativo(){}
-
-    public void retiradaNegativa(){}
+    // public void saldoInsuficiente(){
+    //     ContaMagica c = new ContaMagica();
+    //     c.deposito(6000); 
+    //     c.retirada(10000);
+        
+    // }
 
  }
